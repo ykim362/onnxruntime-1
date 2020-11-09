022d3a166098e5133770d75478519ec51d4cc638
Tianlei Wu <tlwu@microsoft.com>
2020-11-09 11:30:34 -0800

    add operator definition

--- onnxruntime/core/graph/contrib_ops/contrib_defs.cc
+++ onnxruntime/core/graph/contrib_ops/contrib_defs.cc
@@ -554,6 +554,30 @@ and present state are optional. Present state could appear in output even when p
         return;
       });
 
+    static const char* Longformer_Attention_doc = R"DOC(
+Longformer Self Attention with a local context and a global context. Tokens attend locally: Each token
+attends to its W/2 previous tokens and W/2 succeding tokens with W being the window length. A selected few tokens
+attend globally to all other tokens.
+
+The attention_mask is of shape (batch_size, sequence_length), where sequence_length is a multiple of 2W after padding.
+Mask value < 0 means the token is masked, 0 means local attention, and >0 global attention.
+)DOC";
+
+    ONNX_CONTRIB_OPERATOR_SCHEMA(LongformerAttention)
+        .SetDomain(kMSDomain)
+        .SinceVersion(1)
+        .SetDoc(Longformer_Attention_doc)
+        .Attr("num_heads", "Number of attention heads", AttributeProto::INT)
+        .Attr("window", "Attention windows length W", AttributeProto::INT)
+        .Input(0, "input", "3D input tensor with shape (batch_size, sequence_length, hidden_size), hidden_size = num_heads * head_size", "T")
+        .Input(1, "weight", "2D input tensor with shape (hidden_size, 3 * hidden_size)", "T")
+        .Input(2, "bias", "1D input tensor with shape (3 * hidden_size)", "T")
+        .Input(3, "mask", "Attention mask with shape (batch_size, sequence_length)", "M")
+        .Output(0, "output", "3D output tensor with shape (batch_size, append_length, hidden_size)", "T")
+        .TypeConstraint("T", {"tensor(float)", "tensor(float16)"}, "Constrain input and output types to float tensors.")
+        .TypeConstraint("M", {"tensor(int32)"}, "Constrain mask to integer types")
+        .TypeAndShapeInferenceFunction(ONNX_NAMESPACE::propagateShapeAndTypeFromFirstInput);
+
   static const char* EmbedLayerNormalization_ver1_doc = R"DOC(
 EmbedLayerNormalization is the fusion of embedding layer in BERT model, with optional mask processing.
 The embedding layer takes input_ids (word IDs) and segment_ids (sentence IDs) to look up word_embedding, position_embedding,
