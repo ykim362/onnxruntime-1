e81b392eb805e19038f6760c3befa5c20f5760d0
Tianlei Wu <tlwu@microsoft.com>
2020-11-09 17:03:20 -0800

    Add base class for checking inputs

---
+++ onnxruntime/contrib_ops/cpu/bert/longformer_attention_base.h
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "core/common/common.h"
+#include "core/framework/op_kernel.h"
+
+namespace onnxruntime {
+namespace contrib {
+
+class LongformerAttentionBase {
+ protected:
+  LongformerAttentionBase(const OpKernelInfo& info);
+
+  Status CheckInputs(const TensorShape& input_shape,
+                     const TensorShape& weights_shape,
+                     const TensorShape& bias_shape,
+                     const TensorShape& mask_shape) const;
+
+  int num_heads_;           // number of attention heads
+  int window_;              // attention windows length (W)
+};
+
+}  // namespace contrib
+}  // namespace onnxruntime
